8abc1f68d5ec404a51cd7b167ab8d15f638e5107
Seyeon Kim <bbbb56146@gmail.com>
2022-04-21 11:37:19 +0900

    댓글 최신순으로 보이도록

--- commentCreation.js
+++ commentCreation.js
@@ -10,27 +10,38 @@ instaForm.addEventListener("submit", function (e) {
 });
 /*댓글 생성*/
 const addComment = (username, comment) => {
-    const divImg = document.createElement('div');
-    divImg.className = "profile-img";
-    const newImg = document.createElement('img')
-    newImg.src = "https://lh3.googleusercontent.com/a/AATXAJyL7DTh3BXTifLQPj2W_KotPWk2lLRoeKTIfU5l=s96-c"
-    divImg.append(newImg);
+  
+  const divImg = document.createElement('div');
+  divImg.className = "profile-img";
+  
+  const newImg = document.createElement('img')
+  newImg.src = "https://lh3.googleusercontent.com/a/AATXAJyL7DTh3BXTifLQPj2W_KotPWk2lLRoeKTIfU5l=s96-c"
+  
+  divImg.append(newImg);
 
-    const divCommentView = document.createElement("div");
-    divCommentView.className = "comment-view";
+  const divCommentView = document.createElement("div");
+  divCommentView.className = "comment-view";
+
   const newComment = document.createElement("div");
   newComment.className = "comment";
+  
   const bTag = document.createElement("b");
+
   bTag.append(username);
   newComment.append(divImg);
   divCommentView.append(bTag);
   const brTag = document.createElement("br");
   divCommentView.append(brTag)
   divCommentView.append(`${comment}`);
+  
   newComment.append(divCommentView);
+
   const heartTag = document.createElement("div");
   heartTag.className = "button";
   heartTag.id = "heart";
+  
   newComment.append(heartTag);
-commentsContainer.append(newComment);
+  
+  commentsContainer.prepend(newComment);
+
 };
