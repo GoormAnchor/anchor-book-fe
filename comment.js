ce662d97aa3096a7d4f52a62a2703bbe9fe65d24
Seyeon Kim <bbbb56146@gmail.com>
2022-04-23 15:11:56 +0900

    0422

---
+++ comment.js
@@ -0,0 +1,48 @@
+/* 책 id로 댓글 리스트 받아오기 */
+let url = 'http://localhost:8080/api'
+
+
+function getBook(id){
+    const response = fetch("http://localhost:8080/api/comment/find/id?id="+id);
+    return response.then(res => res.json());
+}
+  
+async function exec(id){
+    var text;
+    try {
+        text = await getBook(id);
+        console.log(text);
+        /** inner text로 text 바꾸기 */
+        const bookContainer = document.querySelector(".book-content");
+        
+        const img = bookContainer.querySelector("img");
+        img.src = text.book_image;
+        console.log(img.src);
+
+        const title = bookContainer.querySelector("h2");
+        title.innerText = text.name;
+        console.log(title);
+
+        const info = bookContainer.querySelector(".book-info");
+        console.log(info);
+
+        const bookDate = text.created_at.split('T')[0].split('-');
+        console.log(bookDate);
+        info.innerText = `작가 : ${text.author}
+        출판일 : ${bookDate[0]}.${bookDate[1]}.${bookDate[2]}
+        출판사 : ${text.publisher}
+        가격 : ${text.price}원
+        `;
+        console.log(text.author);
+
+        const aladinLink = bookContainer.querySelector("a");
+        aladinLink.href = text.book_url;
+      
+
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+exec(2);
