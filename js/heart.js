0ddfd833fedd17c3bf094ca6cf5f35e14a1d4301
Seyeon Kim <bbbb56146@gmail.com>
2022-04-25 23:31:01 +0900

    grid로 마이페이지 코멘트 4열 고정

---
+++ js/heart.js
@@ -0,0 +1,43 @@
+const button = document.querySelector(".heart-like-button");
+var bookid = 1;
+var episodeid = null;
+var userseq = 1;
+
+button.addEventListener("click", async () => {
+  var response;
+  if (button.classList.contains("liked")) {
+    button.classList.remove("liked");
+    deleteBookmark(button.id);
+  } else {
+    button.classList.add("liked");
+    response = await postBookmark(bookid, userseq);
+    button.id = response.id;
+  }
+});
+
+/*댓글 삭제(서버) */
+async function deleteBookmark(commentid) {
+  const response = fetch(
+    "http://localhost:8080/api/comment/delete/" + commentid,
+    {
+      method: "DELETE",
+    }
+  );
+}
+
+/*댓글 생성(서버)*/
+async function postBookmark(id, userseq) {
+  const response = fetch("http://localhost:8080/api/comment/createComment", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      user_seq: userseq,
+      content: null,
+      book_id: id,
+    }),
+  });
+
+  return response.then((res) => res.json());
+}
